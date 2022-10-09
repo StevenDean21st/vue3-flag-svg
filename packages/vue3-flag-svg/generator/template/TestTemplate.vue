720094fb2ce0234413f49d6f5fe9b875600c23d1
Ouchdex <3393435537@qq.com>
2022-10-10 02:43:20 +0800

    test: Generate new test and run it

---
+++ packages/vue3-flag-svg/generator/template/TestTemplate.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>Static:</h3>
+  __TEST_TARGET__
+  <h3>Dynamic:</h3>
+  __H_TEST_TEMPLATE_TARGET__
+  <h3>v-for</h3>
+  <component v-for="flag of FlagCodeName" style="width: 100px" :is="flag.component"/>
+</template>
+
+<script setup>
+import { FlagCodeName } from "vue3-flag-svg/manifest";
+__TEST_IMPORT__;
+</script>
+<style scoped></style>
